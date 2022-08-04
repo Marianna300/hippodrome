f2b7fad3d9f9ee74572a99f468df9e0cd52e9fba
Marianna300 <marianna-rgs@yandex.ru>
2022-08-04 12:36:07 +0300

    test and log

--- src/main/java/Horse.java
+++ src/main/java/Horse.java
@@ -1,6 +1,10 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import static java.util.Objects.isNull;
 
 public class Horse {
+    private static final Logger logger = LogManager.getLogger(Horse.class);
 
     private final String name;
     private final double speed;
@@ -8,20 +12,26 @@ public class Horse {
 
     public Horse(String name, double speed, double distance) {
         if (isNull(name)) {
+            logger.error("Horse: Name is null");
             throw new IllegalArgumentException("Name cannot be null.");
         } else if (name.isBlank()) {
+            logger.error("Horse: Name is blank");
             throw new IllegalArgumentException("Name cannot be blank.");
         }
         if (speed < 0) {
+            logger.error("Horse: Speed is negative");
             throw new IllegalArgumentException("Speed cannot be negative.");
         }
         if (distance < 0) {
+            logger.error("Horse: Distance is negative");
             throw new IllegalArgumentException("Distance cannot be negative.");
         }
+
 
         this.name = name;
         this.speed = speed;
         this.distance = distance;
+        logger.debug("Horse: Creation Horse, name {}, speed {}", getName(), getSpeed());
     }
 
     public Horse(String name, double speed) {
@@ -47,4 +57,5 @@ public class Horse {
     public static double getRandomDouble(double min, double max) {
         return (Math.random() * (max - min)) + min;
     }
+
 }
